29a2c813c369ec001d183b70815901b6519d73ab
dingshu <dingshuzisha@gmail.com>
2012-11-05 19:33:29 +0900

    Add named routes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,14 @@
 module StaticPagesHelper
+	# root_path => '/'
+  # root_url  => 'http://localhost:3000/'
+
+	# about_path => '/about'
+	# about_url  => 'http://localhost:3000/about'
+
+	# contact_path => '/contact'
+	# contact_url  => 'http://localhost:3000/contact'
+
+	# help_path => '/help'
+	# help_url  => 'http://localhost:3000/help'
+
 end
